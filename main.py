f7f43e8f04b713ab26f7e5ec99228e04d1e78ba5
turtlecode <85156399+turtlecode@users.noreply.github.com>
2022-08-21 16:23:17 +0300

    Convert PDF Text To AUDIO With Python
    Convert PDF Text To AUDIO With Python

---
+++ main.py
@@ -0,0 +1,14 @@
+import PyPDF2
+import pyttsx3
+
+pdfReader = PyPDF2.PdfFileReader\
+(open("C:\\Users\\firat\\OneDrive\\Masaüstü\\PythonProjects\\news.pdf", 'rb'))
+
+speaker = pyttsx3.init()
+
+for num_page in range(pdfReader.numPages):
+    text = pdfReader.getPage(num_page).extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+
+speaker.stop()
